a1f04700648eabd12592ec3606c7074ee17601f8
Jeffrey Hahn <jef.hahn97@gmail.com>
2025-02-10 21:39:18 +0100

    setup player class and draw methods

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+
 
 
 def main():
@@ -12,19 +14,22 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
     #this while loop is the main game loop, we created a clock object above this to use to setup and limit the fps, the flip method redraws the scene
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt =  clock.tick(60) / 1000
+        
 
 
 
     
-
-
 if __name__ == "__main__":
     main()
